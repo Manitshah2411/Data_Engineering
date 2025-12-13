c62c8429c1bf05f72851fa4be84dc90b73a91361
Manitshah2411 <manitshah42@gmail.com>
2025-12-13 20:48:05 +0530

    env testing done

---
+++ test_env.py
@@ -0,0 +1,12 @@
+from src.config import (
+    DB_USER, DB_NAME, DB_HOST, DB_PASSWORD, DB_PORT, DATABASE_URL
+)
+
+
+print("DB_USER :",DB_USER)
+print("DB_NAME :",DB_NAME)
+print("DB_HOST :",DB_HOST)
+print("DB_PASSWORD :",DB_PASSWORD)
+print("DB_PORT :",DB_PORT)
+print("URL :",DATABASE_URL)
+
