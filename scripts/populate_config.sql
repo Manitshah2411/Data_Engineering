9dcab4bd5e9b9ec711d0d6795d0f21dd3c24e370
Manitshah2411 <manitshah42@gmail.com>
2025-12-28 17:21:58 +0530

    populate config

---
+++ scripts/populate_config.sql
@@ -0,0 +1,15 @@
+INSERT INTO meta.config(pipeline_name,target_schema,target_table,load_type,truncate_before_load,allow_updates,is_active)
+VALUES('fact_daily_sales','warehouse','fact_daily_sales','FULL',TRUE,FALSE,TRUE);
+
+INSERT INTO meta.config(pipeline_name,target_schema,target_table,load_type,truncate_before_load,allow_updates,is_active)
+VALUES('fact_category_performance','warehouse','fact_category_performance','FULL',TRUE,FALSE,TRUE);
+
+INSERT INTO meta.config(pipeline_name,target_schema,target_table,load_type,truncate_before_load,allow_updates,is_active)
+VALUES('fact_customer_lifecycle','warehouse','fact_customer_lifecycle','FULL',TRUE,FALSE,TRUE);
+
+INSERT INTO meta.config(pipeline_name,target_schema,target_table,load_type,truncate_before_load,allow_updates,is_active)
+VALUES('fact_product_performance','warehouse','fact_product_performance','FULL',TRUE,FALSE,TRUE);
+
+INSERT INTO meta.config(pipeline_name,target_schema,target_table,load_type,truncate_before_load,allow_updates,is_active)
+VALUES('sale_performance','warehouse','sale_performance','FULL',TRUE,FALSE,TRUE);
+
