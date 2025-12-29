d72d4bf17fe39aaaded6e413dc9d52cf87cb0a7f
Manitshah2411 <manitshah42@gmail.com>
2025-12-29 17:13:04 +0530

    Final ETL modified for metadata

--- etl/fact_product_performance.py
+++ etl/fact_product_performance.py
@@ -1,39 +1,69 @@
-from src.db_engine import get_engine
 from sqlalchemy import text
+from src.db_engine import get_engine
 from src.utils import log
+from src.metadata import(
+    get_pipeline_config,
+    start_pipeline,
+    end_pipeline_run
+)
 
-def fact_product_performance():
-    engine = get_engine()
-    
-    sql = f"""
-    TRUNCATE TABLE warehouse.fact_product_performance;
-    
-    INSERT INTO warehouse.fact_product_performance
-        SELECT 
-        p.product_id,
-        p.product_category_name,
-        COUNT(DISTINCT o.order_id) AS total_units_sold,
-        SUM(oi.price) AS total_revenue,
-        SUM(oi.price) / COUNT(DISTINCT o.order_id) AS avg_selling_price,
-        MIN(o.order_purchase_timestamp) AS first_sold_date,
-        MAX(o.order_purchase_timestamp) AS last_sold_date
-    FROM warehouse.fact_order_items AS oi
-    JOIN warehouse.dim_products AS p
-        ON p.product_id = oi.product_id
-    JOIN warehouse.dim_orders AS o
-        ON o.order_id = oi.order_id
-    GROUP BY p.product_id, p.product_category_name
-    """
-    
-    with engine.connect() as conn:
-        conn.execute(text(sql))
+PIPELINE_NAME = "fact_product_performance"
+
+config = get_pipeline_config(PIPELINE_NAME)
+run_id = start_pipeline(PIPELINE_NAME)
+engine = get_engine()
+
+try:
+    with engine.begin() as conn:
+        if config['truncate_before_load']:
+            log.info(f"Truncation {PIPELINE_NAME} table")
+            conn.execute(text(f"""
+                         TRUNCATE TABLE {config['target_schema']}.{config['target_table']}
+                         """))
+            
+            log.info("Running SQL Aggregations...")
+            
+            result = conn.execute(text(f"""
+                        INSERT INTO {config['target_schema']}.{config['target_table']}(
+                             product_id,
+                            product_category_name,
+                            total_units_sold,
+                            total_revenue,
+                            avg_selling_price,
+                            first_sold_date,
+                            last_sold_date
+                        )
+                        SELECT 
+                            p.product_id,
+                            p.product_category_name,
+                            COUNT(DISTINCT o.order_id) AS total_units_sold,
+                            SUM(oi.price) AS total_revenue,
+                            SUM(oi.price) / COUNT(DISTINCT o.order_id) AS avg_selling_price,
+                            MIN(o.order_purchase_timestamp) AS first_sold_date,
+                            MAX(o.order_purchase_timestamp) AS last_sold_date
+                        FROM warehouse.fact_order_items AS oi
+                        JOIN warehouse.dim_products AS p
+                        ON p.product_id = oi.product_id
+                        JOIN warehouse.dim_orders AS o
+                        ON o.order_id = oi.order_id
+                        GROUP BY p.product_id,
+                            p.product_category_name
+                                       """))
+            rows_processed = result.rowcount
+            
+        end_pipeline_run(
+            run_id=run_id,
+            status='SUCCESS',
+            rows_processed=rows_processed
+        )
         
-    log.info('Execution completed!!!!')
+except Exception as e:
+    log.info("Pipeline Failed!!!")
     
-
-if __name__ == "__main__":
-    fact_product_performance()
+    end_pipeline_run(
+        run_id=run_id,
+        status='FAILED',
+        error_message=str(e)
+    )
     
-    
-    
-    
+    raise
--- etl/sale_performance.py
+++ etl/sale_performance.py
@@ -1,35 +1,69 @@
-from src.db_engine import get_engine
 from sqlalchemy import text
+from src.db_engine import get_engine
 from src.utils import log
+from src.metadata import(
+    get_pipeline_config,
+    start_pipeline,
+    end_pipeline_run
+)
 
-def sale_performance():
-    engine = get_engine()
-    
-    sql = f"""
-    TRUNCATE TABLE warehouse.sale_performance;
-    
-    INSERT INTO warehouse.sale_performance
-    SELECT 
-        oi.seller_id,
-        COUNT(DISTINCT o.order_id) AS total_orders,
-        SUM(oi.price) AS total_revenue,
-        ROUND(SUM(oi.price) / COUNT(DISTINCT o.order_id),2) AS avg_order_value,
-        COUNT(DISTINCT p.product_id) AS total_items_sold,
-        MIN(o.order_purchase_timestamp) AS first_sales_date,
-        MAX(o.order_purchase_timestamp) AS last_sales_date
-    FROM warehouse.fact_order_items AS oi
-    JOIN warehouse.dim_products AS p
-    ON p.product_id = oi.product_id
-    JOIN warehouse.dim_orders AS o
-    ON o.order_id = oi.order_id
-    GROUP BY oi.seller_id
-    ORDER BY COUNT(DISTINCT o.order_id) DESC;
-    """
-    
-    with engine.connect() as conn:
-        conn.execute(text(sql))
+PIPELINE_NAME = "sale_performance"
+
+config = get_pipeline_config(PIPELINE_NAME)
+run_id = start_pipeline(PIPELINE_NAME)
+engine = get_engine()
+
+try:
+    with engine.begin() as conn:
+        if config['truncate_before_load']:
+            log.info(f"Truncation {PIPELINE_NAME} table")
+            conn.execute(text(f"""
+                         TRUNCATE TABLE {config['target_schema']}.{config['target_table']}
+                         """))
+            
+            log.info("Running SQL Aggregations...")
+            
+            result = conn.execute(text(f"""
+                        INSERT INTO {config['target_schema']}.{config['target_table']}(
+                            seller_id,
+                            total_orders,
+                            total_revenue,
+                            avg_order_value,
+                            total_items_sold,
+                            first_sales_date,
+                            last_sales_date
+                        )
+                        SELECT 
+                            oi.seller_id,
+                            COUNT(DISTINCT o.order_id) AS total_orders,
+                            SUM(oi.price) AS total_revenue,
+                            ROUND(SUM(oi.price) / COUNT(DISTINCT o.order_id),2) AS avg_order_value,
+                            COUNT(DISTINCT p.product_id) AS total_items_sold,
+                            MIN(o.order_purchase_timestamp) AS first_sales_date,
+                            MAX(o.order_purchase_timestamp) AS last_sales_date
+                        FROM warehouse.fact_order_items AS oi
+                        JOIN warehouse.dim_products AS p
+                        ON p.product_id = oi.product_id
+                        JOIN warehouse.dim_orders AS o
+                        ON o.order_id = oi.order_id
+                        GROUP BY oi.seller_id
+                        ORDER BY COUNT(DISTINCT o.order_id) DESC;
+                                       """))
+            rows_processed = result.rowcount
+            
+        end_pipeline_run(
+            run_id=run_id,
+            status='SUCCESS',
+            rows_processed=rows_processed
+        )
         
-    log.info('Executed Successfully!!!')
+except Exception as e:
+    log.info("Pipeline Failed!!!")
     
-if __name__ == "__main__":
-    sale_performance()
+    end_pipeline_run(
+        run_id=run_id,
+        status='FAILED',
+        error_message=str(e)
+    )
+    
+    raise
