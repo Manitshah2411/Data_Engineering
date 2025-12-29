28ea089c06856858416a422ed03d0a9d5ccfabd5
Manitshah2411 <manitshah42@gmail.com>
2025-12-29 16:31:31 +0530

    For metadata config and etl_runs metadata_insertion

--- etl/fact_category_performance.py
+++ etl/fact_category_performance.py
@@ -1,33 +1,65 @@
-from src.db_engine import get_engine
 from sqlalchemy import text
+from src.db_engine import get_engine
+from src.metadata import(
+    get_pipeline_config,
+    start_pipeline,
+    end_pipeline_run
+)
 from src.utils import log
 
-def fact_category_performance():
-    engine = get_engine()
+PIPELINE_NAME = "fact_category_performance"
+
+engine = get_engine()
+config = get_pipeline_config(PIPELINE_NAME)
+run_id = start_pipeline(PIPELINE_NAME)
+
+try:
+    with engine.begin() as conn:
+        if config['truncate_before_load']:
+            log.info(f"Truncation {PIPELINE_NAME} table")
+            conn.execute(text(f"""
+                         TRUNCATE TABLE {config['target_schema']}.{config['target_table']}
+                         """))
+            
+            log.info("Running SQL Aggregations...")
+            
+            result = conn.execute(text(f"""
+                        INSERT INTO {config['target_schema']}.{config['target_table']}(
+                            product_category_name,
+                            total_revenue,
+                            total_units_sold,
+                            num_products,
+                            avg_price
+                         )
+                        SELECT 
+                            p.product_category_name,
+                            SUM(oi.price) AS total_revenue,
+                            COUNT(DISTINCT o.order_id) AS total_units_sold,
+                            COUNT(DISTINCT p.product_id) AS num_products,
+                            ROUND(AVG(oi.price),2) AS avg_price
+                        FROM warehouse.fact_order_items AS oi
+                        JOIN warehouse.dim_orders AS o
+                        ON o.order_id = oi.order_id
+                        JOIN warehouse.dim_products AS p
+                        ON p.product_id = oi.product_id
+                        GROUP BY p.product_category_name;
+                         """))
+            rows_processed = result.rowcount
+            
+            end_pipeline_run(
+                run_id=run_id,
+                status='SUCCESS',
+                rows_processed=rows_processed
+            )
+        
+except Exception as e:
+    log.info('Pipeline failed!!!')
     
-    sql = f"""
-    TRUNCATE TABLE warehouse.fact_category_performance;
+    end_pipeline_run(
+        run_id=run_id,
+        status='FAILED',
+        error_message=str(e)
+    )
     
-    INSERT INTO warehouse.fact_category_performance
-    SELECT 
-        p.product_category_name,
-        SUM(oi.price) AS total_revenue,
-        COUNT(DISTINCT o.order_id) AS total_units_sold, -- Also same as num_orders as here the quantity is always 1
-        COUNT(DISTINCT p.product_id) AS num_products,
-        ROUND(AVG(oi.price),2) AS avg_price
-    FROM warehouse.fact_order_items AS oi
-    JOIN warehouse.dim_orders AS o
-    ON o.order_id = oi.order_id
-    JOIN warehouse.dim_products AS p
-    ON p.product_id = oi.product_id
-    GROUP BY p.product_category_name;
-    """
-    
-    with engine.connect() as conn:
-        conn.execute(text(sql))
-    
-    log.info('Executed!!!')    
-    
-if __name__ == "__main__":
-    fact_category_performance()
-    
+    raise
+
