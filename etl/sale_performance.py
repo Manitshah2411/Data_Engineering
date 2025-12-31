48153e40b48d6d235046738881be021a5b016806
Manitshah2411 <manitshah42@gmail.com>
2025-12-31 21:39:26 +0530

    Default Fixes

--- etl/sale_performance.py
+++ etl/sale_performance.py
@@ -26,7 +26,7 @@ try:
                         COUNT(DISTINCT o.order_id) AS total_orders,
                         SUM(oi.price) AS total_revenue,
                         ROUND(SUM(oi.price) / COUNT(DISTINCT o.order_id),2) AS avg_order_value,
-                        COUNT(DISTINCT p.product_id) AS total_items_sold,
+                        COUNT(*) AS total_items_sold,
                         MIN(o.order_purchase_timestamp) AS first_sales_date,
                         MAX(o.order_purchase_timestamp) AS last_sales_date
                     FROM warehouse.fact_order_items AS oi
@@ -37,7 +37,6 @@ try:
                     WHERE (:last_watermark) IS NULL
                     OR o.order_purchase_timestamp > :last_watermark
                     GROUP BY oi.seller_id
-                    ORDER BY COUNT(DISTINCT o.order_id) DESC
                     )
                     INSERT INTO {config['target_schema']}.{config['target_table']} AS sp(
                         seller_id,
